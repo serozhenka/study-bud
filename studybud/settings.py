d34918cccd9cfea26671bad47f5c829a2fb93d0f
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-03-09 00:24:56 +0200

    modified settings

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eg-rtf_sx@!*ks#gvo==ko2_26^ab((x6cxhib=8$-k($69p#7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['study-bud-project.herokuapp.com']
 
